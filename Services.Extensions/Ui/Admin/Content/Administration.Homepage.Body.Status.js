5c8f9cfe5c8f615fcfa46961dfde960d8385d160
Alexander Mantzoukas <ama@trustpilot.com>
2014-08-10 13:07:30 +0200

    Add font files and update css

---
+++ Services.Extensions/Ui/Admin/Content/Administration.Homepage.Body.Status.js
@@ -0,0 +1,12 @@
+﻿{
+    ready: function() {
+        var self = this;
+        setInterval(function() {
+            self.AdminRows.Get(null, function() {
+                // Should load databound values in UI
+            }, function() {
+                self.AdminError.$.show();
+            });
+        }, 1000);
+    }
+}
