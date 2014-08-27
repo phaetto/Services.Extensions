a0078425e6b06c4f6b47fa39bc1967a58d211fd5
Alexander Mantzoukas <ama@trustpilot.com>
2014-08-27 18:31:51 +0200

    Adding the two persistent stores to admin

--- Services.Extensions/Ui/AdministrationServer.cs
+++ Services.Extensions/Ui/AdministrationServer.cs
@@ -1,22 +1,41 @@
 ﻿namespace Services.Extensions.Ui
 {
+    using System;
+    using System.IO;
     using Chains;
+    using Chains.Persistence;
     using Chains.Play.Web;
     using Chains.Play.Web.HttpListener;
+    using Services.Extensions.Contexts.ServiceAutoManager;
     using Services.Extensions.Contexts.ServiceStarter;
     using Services.Extensions.Ui.Modules;
     using Services.Management.Administration.Worker;
 
-    public sealed class AdministrationServer : Chain<AdministrationServer>
+    public sealed class AdministrationServer : Chain<AdministrationServer>, IDisposable
     {
         private const int HttpServerThreads = 3;
 
         private readonly HttpServer httpServer;
 
-        private readonly ServiceStarterContext serviceStarterContext = new ServiceStarterContext();
+        private readonly PersistentServiceStarterContext persistentServiceStarterContext;
+
+        private readonly PersistentAutoServiceContext persistentAutoServiceContext;
 
         public AdministrationServer(string hostname, int port, WorkUnitContext workUnitContext)
         {
+            persistentServiceStarterContext = new PersistentServiceStarterContext(
+                new PersistentServiceStarterData
+                {
+                    Id = PersistentServiceStarterContext.DefaultIdForStartingOptions
+                },
+                new FilePersistentStore<PersistentServiceStarterData>(
+                    string.Format(PersistentServiceStarterContext.DataFolderUnformatted, Path.DirectorySeparatorChar)));
+
+            persistentAutoServiceContext =
+                new PersistentAutoServiceContext(
+                    new FilePersistentStoreWithMemorySnapshotCache<PersistentAutoServiceData>(
+                        string.Format(PersistentAutoServiceContext.DataFolderUnformatted, Path.DirectorySeparatorChar)));
+
             httpServer =
                 new ServerHost(hostname, port, AdministratorHttpRequestHandler.AdminHomepageUriPath).Do(
                     new StartHttpServer(new string[0], HttpServerThreads));
@@ -25,6 +44,17 @@
 
             // string.Format("{0}Ui{1}Admin{1}Content{1}", AppDomain.CurrentDomain.BaseDirectory, Path.DirectorySeparatorChar)
             new FileSystemHttpRequestHandler(httpServer);
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                httpServer.Stop();
+            }
+            catch (Exception)
+            {
+            }
         }
     }
 }
