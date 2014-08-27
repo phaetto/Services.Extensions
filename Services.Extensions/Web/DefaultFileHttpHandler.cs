6552ffdf0157c35440991dda83488e15bda6a578
Alexander Mantzoukas <ama@trustpilot.com>
2014-08-27 09:57:21 +0200

    Add default-file and redirect http handlers

---
+++ Services.Extensions/Web/DefaultFileHttpHandler.cs
@@ -0,0 +1,46 @@
+﻿namespace Services.Extensions.Web
+{
+    using System;
+    using System.IO;
+    using System.Net;
+    using Chains.Play.Web;
+    using Chains.Play.Web.HttpListener;
+
+    public sealed class DefaultFileHttpHandler : HttpRestHandler
+    {
+        private readonly string defaultFile;
+
+        private readonly string directory;
+
+        public DefaultFileHttpHandler(HttpServer httpServer, string defaultFile = "index.html", string path = "/", string directory = null)
+            : base(httpServer, path, defaultFile)
+        {
+            this.defaultFile = defaultFile;
+            this.directory = directory;
+        }
+
+        public override void Get(HttpListenerContext context)
+        {
+            var path = string.Format("{0}{1}{2}", directory, Path.DirectorySeparatorChar, defaultFile);
+
+            new HttpResultContext().SendFile(path)
+                .AccessControlAllowOriginAll()
+                .ApplyOutputToHttpContext(context);
+        }
+
+        public override void Post(HttpListenerContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Put(HttpListenerContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Delete(HttpListenerContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services.Extensions/Web/RedirectHttpHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace Services.Extensions.Web
+{
+    using System.Net;
+    using Chains;
+    using Chains.Play.Web;
+    using Chains.Play.Web.HttpListener;
+
+    public sealed class RedirectHttpHandler : Chain<RedirectHttpHandler>, IHttpRequestHandler
+    {
+        private readonly string redirectAddressTo;
+
+        public RedirectHttpHandler(HttpServer svfServer, string redirectAddressTo, string path = "/")
+        {
+            this.redirectAddressTo = redirectAddressTo;
+            svfServer.Modules.Add(this);
+            svfServer.AddPath(path);
+        }
+
+        public bool ResolveRequest(HttpListenerContext context)
+        {
+            new HttpResultContext(redirectAddressTo, false).ApplyOutputToHttpContext(context);
+
+            return true;
+        }
+    }
+}
